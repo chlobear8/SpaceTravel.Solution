3638202a76b76c4b1c97ce37e311d3773bab4b79
Chloe ONeil <chloesporri@yahoo.com>
2023-04-30 12:53:27 -0700

    add destination controller

---
+++ SpaceTravel/Controllers/DestinationController.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using SpaceTravel.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpaceTravel.Controllers
+{
+    public class DestinationController : Controller
+    {
+        private readonly SpaceTravelContext _db;
+
+        public DestinationController(SpaceTravelContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Destination> model = _db.Destination.ToList();
+            return View(model);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var destination = await _db.Destination
+                .FirstOrDefaultAsync(m => m.DestinationId == id);
+            if (destination == null)
+            {
+                return NotFound();
+            }
+
+            return View(destination);
+        }
+
+    }
+}
